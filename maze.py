6793d6dd0760f64eb8fab6e60cceebcd6c4847a6
viniciusroriz <fowardshift@gmail.com>
2023-09-14 23:24:32 -0300

    progresso

--- maze.py
+++ maze.py
@@ -1,75 +1,136 @@
 import cv2
-import networkx as nx
-import matplotlib.pyplot as plt
+
 
 class Node:
-    x : int
-    y : int
-    parent : str
-    neighbors: list
-
     def __init__(self, x, y, parent) -> None:
         self.x = x
         self.y = y
         self.parent = parent
-    
+        self.neighbors = []
 
-def read_image():
-    maze_image = cv2.imread('maze.png', cv2.IMREAD_GRAYSCALE)
-    _, binary_image = cv2.threshold(maze_image, 128, 255, cv2.THRESH_BINARY)
-    return binary_image
+
+class Graph:
+    def __init__(self) -> None:
+        self.nodes = []
+
+    def add_node(self, node: Node) -> Node:
+        self.nodes.append(node)
+        return node
+
+    def add_edge(self, node1, node2):
+        if node1 not in self.nodes or node2 not in self.nodes:
+            raise ValueError("Both nodes must be in the graph.")
+
+        node1.neighbors.append(node2)
+        node2.neighbors.append(node1)
+
+    def __str__(self):
+        result = ""
+        for node in self.nodes:
+            neighbors = ", ".join(
+                f"({neighbor.x},{neighbor.y})" for neighbor in node.neighbors
+            )
+            result += f"({node.x},{node.y}) -> [{neighbors}]\n"
+        return result
+
+
+
+def move(pos: list, direction: str) -> None:
+    x, y = pos
+    if direction == "N":
+        y -= 1
+    if direction == "S":
+        y += 1
+    if direction == "W":
+        x -= 1
+    if direction == "E":
+        x += 1
+
+    return [x,y]
+
+def find_dir(parent) -> str:
+        if parent == "N":
+            return "S"
+        if parent == "S":
+            return "N"
+        if parent == "E":
+            return "W"
+        if parent == "W":
+            return "E"
+
+
+def is_path(pos: list):
+    return True if maze_image[pos[1], pos[0]] == 255 else False
+
+
+def scan(pos: list, back: str):
+    directions = ["N", "S", "W", "E"]
+    directions.remove(back)
+    available = []
+
+    for d in directions:
+        if is_path(move(pos, d)):
+            available.append(d)
+
+    return available
+
+
+maze_image = cv2.imread("maze.png", cv2.IMREAD_GRAYSCALE)
+
+def create_node(maze_graph, c_pos, last_node, last_direction, scn, corner):
+    c_pos = move([last_node.x, last_node.y], scn[0])
+    new_node = Node(x=c_pos[0], y=c_pos[1], parent=find_dir(last_direction)  if corner else last_direction)
+    maze_graph.add_node(new_node)
+    maze_graph.add_edge(last_node, new_node)
+    last_node = new_node
+    c_pos = [last_node.x, last_node.y]
 
 def create_graph():
-    binary_image = read_image()
-    maze_graph = list()
-    height, width = binary_image.shape
+    maze_graph = Graph()
+    height, width = maze_image.shape
 
     for x in range(width):
-        if binary_image[0, x] == 255:
-            start = Node(x=x, y=0, parent='N')
+        if maze_image[0, x] == 255:
+            start = Node(x=x, y=0, parent="N")
 
-        if binary_image[height-1, x] == 255:
-            end = Node(x=x, y=height-1, parent='N')
+        if maze_image[height - 1, x] == 255:
+            end = Node(x=x, y=height - 1, parent="N")
 
-    maze_graph.append(start)    
-    maze_graph.append(end)
+    maze_graph.add_node(start)
+    maze_graph.add_node(end)
 
-    print(f'start = {start.x}, end = {end.x}')
+    print(f"start = ({start.x},{start.y}), end = ({end.x},{end.y})\n")
 
-    c_pos = start
-    # c_pos = start
+    c_pos = [start.x, start.y]
+    last_direction = "N"
+    last_node = start
 
-    # north = (-1,0)
-    # east = (0,-1)
-    # west = (0,1)
-    # south = (1,0)
+    i = 0
+    while i < 4:
+        scn = scan(c_pos, last_direction)
+        if len(scn) == 1:
+            if scn != find_dir(last_direction): # If the path changes directions (corner)
+                create_node(maze_graph, c_pos, last_node, last_direction, scn, corner=True)
+            else:
+                c_pos = move(c_pos, scn)
+        i += 1
 
-    # c_pos = tuple(map(lambda a: sum(a), zip(c_pos, south)))
-    # y, x = c_pos
-    # print(height-y, x)
-    # if binary_image[y, x] == 255:
-    #     maze_graph.add_node('second', pos=(x, height-y))
-    #     print("entrei")
+
+# add new node
+    # c_pos = move([last_node.x, last_node.y], scn[0])
+    # new_node = Node(x=c_pos[0], y=c_pos[1], parent=last_direction)
+    # maze_graph.add_node(new_node)
+    # maze_graph.add_edge(last_node, new_node)
+    # last_node = new_node
+    # c_pos = [last_node.x, last_node.y]
+
+    # elif len(scn) > 2:
+    #     new_node = Node(x=c_pos[0], y=c_pos[1], parent=direc.find(last_direction))
+    #     maze_graph.add_node(new_node)
+    #     maze_graph.add_edge(last_node, new_node)
+    #     last_node = new_node
+
+    print(maze_graph)
 
 
 create_graph()
-# while c_pos != end:
-    
-
-# for y in range(height):
-#     for x in range(width):
-#         if binary_image[y, x] == 255:  # Paths are white pixels (255)
-#             maze_graph.add_node((x, y))
-#             for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-#                 nx, ny = x + dx, y + dy
-#                 if 0 <= nx < width and 0 <= ny < height and binary_image[ny, nx] == 0:
-#                     maze_graph.add_edge((x, y), (nx, ny))
-
-#pos = {node: node for node in maze_graph.nodes()}
-#pos = {node:(x, y) for (node, (x,y)) in nx.get_node_attributes(maze_graph, 'pos').items()}
-# pos = nx.get_node_attributes(maze_graph, 'pos')
-# #pos = {node: (y, x) for node, (x, y) in pos.items()}
-
-# nx.draw(maze_graph, pos, with_labels=True, node_size=500, node_color='skyblue', font_size=10)
-# # nx.draw(maze_graph, pos, with_labels=False, node_size=10)
-# plt.show()
